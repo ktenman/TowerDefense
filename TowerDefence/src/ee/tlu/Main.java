0ca5788f8c327b15162f2ae7841a13f592fd77b5
Konstantin <ktenman@gmail.com>
2015-05-09 18:16:27 +0300

    Main window created and popup is working.

--- TowerDefence/src/ee/tlu/Main.java
+++ TowerDefence/src/ee/tlu/Main.java
@@ -1,9 +1,23 @@
 package ee.tlu;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
 public class Main {
+	
+	public Main(){
+		Display.setTitle("Mäng");
+		try {
+			Display.setDisplayMode(new DisplayMode(600, 400));
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public static void main(String[] args) {
-		
+		new Main();
 	}
 
 }
